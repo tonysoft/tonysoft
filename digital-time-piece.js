d4832d4b6ccf9fb792b3de366bfca1183cd4241f
Tony Sukiennik <tony@irelate.us>
2019-06-27 15:34:52 -0700

    test unpkg?module to simplify dependency resolution................

--- digital-time-piece.js
+++ digital-time-piece.js
@@ -1,5 +1,5 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import {digitCell} from 'tonysoft/digit-cell.js';
+import {digitCell} from 'digit-cell/digit-cell.js';
 
 /**
  * `digital-time-piece`
