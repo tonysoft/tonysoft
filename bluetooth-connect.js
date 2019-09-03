2004b690e1386f65f7356c604320fa68b2f424e3
tonysoft <tony@irelate.us>
2019-09-03 16:16:47 -0700

    bluetooth-connect almost ready for builder

--- bluetooth-connect.js
+++ bluetooth-connect.js
@@ -40,12 +40,19 @@ class BluetoothConnect extends PolymerElement {
         serviceId: {
             type: String
         },
-        characteristics: {
+        getCharacteristics: {
+            type: Array
+        },
+        putCharacteristics: {
             type: Array
         },
         config: {
             type: Object,
             observer: "_config"
+        },
+        configUrl: {
+            type: String,
+            observer: "_configUrl"
         },
         connect: {
             type: Boolean,
@@ -71,13 +78,15 @@ class BluetoothConnect extends PolymerElement {
       super();
       this.deviceName = "";
       this.serviceId = "";
-      this.characteristics = null;
+      this.getCharacteristics = [];
+      this.putCharacteristics = [];
       this.config = null;
       this.connectUi = false;
       this.connect = false;
       this.disconnect = false;
       this.isReady = false;
       this.deviceConnected = false;
+      this.configUrl = "";
     }
 
     ready() {
@@ -86,6 +95,53 @@ class BluetoothConnect extends PolymerElement {
         context.isReady = true;
     }
 
+    _configUrl(newValue) {
+        var context = this;
+        if (newValue) {
+            fetch(newValue)
+            .then(res => res.json())
+            .then(spec => context.setConfig(spec))
+            .catch(err => console.error(err));
+         }
+    }
+
+    setConfig(config) {
+        var context = this;
+        context.config = config;
+    }
+
+    _config(config) {
+        var context = this;
+        if (config) {
+            if (config.deviceName) {
+                context.deviceName = config.deviceName;
+            }
+            if (config.serviceId) {
+                context.serviceId = config.serviceId;
+            }
+            var getIndex = 0;
+            var putIndex = 0;
+            for (var name in config.characteristics) {
+                var characteristic = config.characteristics[name];
+                if (characteristic.get) {
+                    characteristic.index = getIndex;
+                    characteristic.name = name;
+                    getIndex++;
+                    context.getCharacteristics.push(characteristic);
+                }
+                if (characteristic.put) {
+                    characteristic.index = putIndex;
+                    characteristic.name = name;
+                    putIndex++;
+                    context.putCharacteristics.push(characteristic);
+                }
+            };
+            context.dispatchEvent(new CustomEvent("configParsed", { 
+                detail: config
+            }));
+        }
+    }
+  
     _connect() {
         var context = this;
         if (context.connect) {
@@ -163,58 +219,141 @@ class BluetoothConnect extends PolymerElement {
         context.connect = false;
         if (context.bluetoothDevice) {
             context.bluetoothDevice = undefined;
-            // return context.connectDeviceAndCacheCharacteristics();
+            // return context.connectDevice();
         }
         console.log('Requesting Bluetooth Device(s)...');
         var acceptAllDevices = context.deviceName ? undefined : true;
-        var filters = context.deviceName ? [{ name: [context.deviceName] }] : undefined;
+        var filters = context.deviceName ? [{ namePrefix: [context.deviceName] }] : undefined;
+        var optionalServices = context.serviceId ? [context.serviceId] : undefined;
         return navigator.bluetooth.requestDevice({
             acceptAllDevices: acceptAllDevices,
-            filters: filters})
+            filters: filters,
+            optionalServices: optionalServices})
         .then(device => {
             context.bluetoothDevice = device;
             context.bluetoothDevice.context = context;
             context.bluetoothDevice.addEventListener('gattserverdisconnected', context.onBluetoothConnectDisconnected);
-            context.connectDeviceAndCacheCharacteristics().then(response => {
-                console.log("Connected!");
-                context.deviceConnected = true;
-                context.dispatchEvent(new CustomEvent("connected", { 
-                    detail: { "connected": context.deviceConnected, "deviceName": context.deviceName }
-                }));
-            })
-            .catch(error => {
-                console.log('Error connect! ' + error);
-            });
+            context.connectDevice();
         })
         .catch(error => {
             console.log('Error requestDevice! ' + error);
         });
     }
 
-    connectDeviceAndCacheCharacteristics() {
+    connectDevice() {
         var context = this;
         if (context.bluetoothDevice.gatt.connected) {
           return Promise.resolve();;
         }
       
         console.log('Connecting to GATT Server...');
-        return context.bluetoothDevice.gatt.connect()
+        return context.bluetoothDevice.gatt.connect().then(server => {
+            console.log("Connected!");
+            context.deviceConnected = true;
+            context.dispatchEvent(new CustomEvent("connected", { 
+                detail: { "connected": context.deviceConnected, "deviceName": context.deviceName }
+            }));
+            context.retrieveService(server);
+        })
         .catch(error => {
             console.log('Error Connect! ' + error);
         });
-}
+    }
 
+    retrieveService(server) {
+        var context = this;
+        if (context.serviceId) {
+            server.getPrimaryService(context.serviceId)
+            .then(service => {
+                console.log("Service Retrieved!");
+                var context = service.device.context;
+                context.setupCharacteristics(service);
+            })
+            .catch(error => {
+                console.log('Service Retrieval Error! ' + error);
+            });
+        }
+    }
+
+    setupCharacteristics(service) {
+        var context = this;
+        if (context.getCharacteristics.length) {
+            context.getCharacteristics.forEach(function(rawCharacteristic) {
+                service.getCharacteristic(rawCharacteristic.id).then(characteristic => { context.setupGetCharacteristic(rawCharacteristic, characteristic) });
+            })
+        }
+        if (context.putCharacteristics.length) {
+            context.putCharacteristics.forEach(function(rawCharacteristic) {
+                service.getCharacteristic(rawCharacteristic.id).then(characteristic => { context.setupPutCharacteristic(rawCharacteristic, characteristic) });
+            })
+        }
+    }
+
+    setupGetCharacteristic(rawCharacteristic, characteristic) {
+        var context = this;
+        rawCharacteristic.characteristic = characteristic;
+        characteristic.properties.name = rawCharacteristic.name;
+        characteristic.addEventListener('characteristicvaluechanged', context.handleCharacteristicChanged);
+        if (rawCharacteristic.notifications) {
+            characteristic.startNotifications()
+            .then(_ => {
+                console.log('> Notifications started');
+            })
+            .catch(error => {
+                console.log('Error startNotifications! ' + error);
+            });
+        }
+        return characteristic.readValue();
+    }
+    
+    setupPutCharacteristic(rawCharacteristic, characteristic) {
+        rawCharacteristic.characteristic = characteristic;
+    }
+
+    handleCharacteristicChanged(event) {
+        var context = event.srcElement.service.device.context;
+        var characteristic = event.srcElement;
+        var value = event.target.value;
+        var dataArray = [];
+        for (var i = 0; i < value.byteLength; i++) {
+            dataArray.push(value.getUint8(i));
+        }
+        var dataString = context.uintArrayAsString(dataArray);
+        console.log('> Value is ' + dataString);
+        context.dispatchEvent(new CustomEvent("characteristicChanged", { 
+            detail: { "characteristic": characteristic.properties.name, "data": dataString }
+        }));
+    } 
+    
+
+    dataAsUint8Array(charList) {
+        var context = this;
+        var uintArray = [];
+        for (var i = 0; i < charList.length; i++) {
+            uintArray.push(charList[i].charCodeAt(0));
+        }
+        let data = new Uint8Array(uintArray);
+        return data;
+    }
+    
+    uintArrayAsString(uintArray) {
+        var context = this;
+        var dataString = String.fromCharCode.apply(null, uintArray);
+        return dataString;
+    }
+    
+    
     onBluetoothConnectDisconnected(e) {
         var context = e.currentTarget.context;
         console.log('> Bluetooth Device disconnected');
-//        reconnect();
         context.bluetoothDevice = null;
         context.deviceConnected = false;
         context.dispatchEvent(new CustomEvent("connected", { 
             detail: { "connected": context.deviceConnected, "deviceName": context.deviceName }
         }));
+//        reconnect();  // Auto reconnect
         function reconnect() {
-            context.connectDeviceAndCacheCharacteristics().then(response => {
+            context.connectDevice().then(response => {
                   console.log("Reconnected!")
               })
               .catch(error => {
@@ -222,20 +361,6 @@ class BluetoothConnect extends PolymerElement {
               });
         }
     }
-}
-window.onBluetoothConnectDisconnected = function(e) {
-    var context = e.currentTarget.context;
-    console.log('> Bluetooth Device disconnected');
-    // reconnect();
-    // function reconnect() {
-    //       connectDeviceAndCacheCharacteristics().then(response => {
-    //           reconnected = true;
-    //           log("Reconnected!")
-    //       })
-    //       .catch(error => {
-    //           log('Error reconnect! ' + error);
-    //       });
-    // }
 }
 
 window.customElements.define('bluetooth-connect', BluetoothConnect);
