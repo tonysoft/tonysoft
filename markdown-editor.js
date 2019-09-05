5c649fa18ef454c021414ba00445e7694cc877b7
tonysoft <tony@irelate.us>
2019-09-05 08:32:38 -0700

    prev

--- markdown-editor.js
+++ markdown-editor.js
@@ -19,7 +19,6 @@ class MarkdownEditor extends PolymerElement {
             .main {
                 border: 1px solid black;
                 cursor: pointer;
-                position: relative;
             }
             #preview {
                 padding: 10px;
