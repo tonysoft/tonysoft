eb278e9705dc6b47221177685bb1c51467d2e7be
tonysoft <tony@irelate.us>
2020-05-11 20:02:13 -0700

    pdf-viewer no clip

--- pdf-viewer.js
+++ pdf-viewer.js
@@ -30,7 +30,7 @@ class PdfViewer extends PolymerElement {
             }
 
         </style>
-        <div id="editor" on-click="focusOnEditor" class="main noSelect" style="width: [[setWidth(width)]]; max-width: [[setMaxWidth(maxWidth)]]; height: [[setHeight(height)]]; overflow: hidden;">
+        <div id="editor" on-click="focusOnEditor" class="main noSelect" style="width: [[setWidth(width)]]; max-width: [[setMaxWidth(maxWidth)]]; height: [[setHeight(height)]]; overflow: visible;">
             <a href="[[src]]" title="View Document in a New Tab" style="position: relative; pointer-events: [[hasBrowserLink(browserLink)]];" target="_blank">
                 <canvas id="the-canvas" class="border" style="display: none; margin: [[margin]]px; position: absolute; left: 0px; top: 0px; width: 100%; height: 100%; " ></canvas>
             </a>
