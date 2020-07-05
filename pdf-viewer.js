b7471622054075d45c4d8ce40e9e78981a868410
tonysoft <tony@irelate.us>
2020-07-05 15:01:25 -0700

    pdf-viewer border width 1.5px

--- pdf-viewer.js
+++ pdf-viewer.js
@@ -21,7 +21,7 @@ class PdfViewer extends PolymerElement {
                 position: relative;
             }
             .border {
-                border: 1px solid #888888 !important;
+                border: 1.5px solid #888888 !important;
                 border-radius: 5px;
             }
             #preview {
@@ -398,29 +398,29 @@ class PdfViewer extends PolymerElement {
             canvas.width = viewport.width;
 
             if (canvas.width > canvas.height) {
-                var contWidth = context.baseWidth - 2; // - context.margin;
-                var contHeight = parseInt(context.baseWidth * canvas.height / canvas.width) - 2;// - context.margin;
+                var contWidth = context.baseWidth; // - context.margin;
+                var contHeight = parseInt(context.baseWidth * canvas.height / canvas.width);// - context.margin;
                 if (contHeight > context.baseHeight) {
-                    contHeight = context.baseHeight - 2; // - 2;
-                    contWidth = parseInt(contHeight * canvas.width / canvas.height) - 2; // - context.margin;
-                    canvas.style.top = "1px";
+                    contHeight = context.baseHeight; // - 2;
+                    contWidth = parseInt(contHeight * canvas.width / canvas.height); // - context.margin;
+                    canvas.style.top = "0px";
                     canvas.style.left = Math.max(((context.baseWidth - contWidth) / 2)) + "px";
                 } else {
-                    canvas.style.left = "1px";
+                    canvas.style.left = "0px";
                     canvas.style.top = Math.max(((context.baseHeight - contHeight) / 2), 0) + "px";
                 }
                 canvas.style.width = contWidth + "px";
                 canvas.style.height = contHeight + "px";
             } else {
-                var contWidth = parseInt(context.baseHeight * canvas.width / canvas.height) - 2; // - context.margin;
-                var contHeight = context.baseHeight - 2; // - context.margin;
+                var contWidth = parseInt(context.baseHeight * canvas.width / canvas.height); // - context.margin;
+                var contHeight = context.baseHeight; // - context.margin;
                 if (contWidth > context.baseWidth) {
-                    contWidth = context.baseWidth - 2; // - 2;
-                    contHeight = parseInt(contWidth * canvas.height / canvas.width) - 2; // - context.margin;
-                    canvas.style.left = "1px";
+                    contWidth = context.baseWidth; // - 2;
+                    contHeight = parseInt(contWidth * canvas.height / canvas.width); // - context.margin;
+                    canvas.style.left = "0px";
                     canvas.style.top = Math.max(((context.baseHeight - contHeight) / 2), 0) + "px";
                 } else {
-                    canvas.style.top = "1px";
+                    canvas.style.top = "0px";
                     canvas.style.left = Math.max(((context.baseWidth - contWidth) / 2), 0) + "px";
                 }
                 canvas.style.width = contWidth + "px";
