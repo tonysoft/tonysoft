dbaf0ed2cdbf43acb001a1296da40af3c8ad81fd
tonysoft <tony@irelate.us>
2021-04-08 15:28:14 -0400

    add dependencies for pdf.js

--- pdf-viewer2.js
+++ pdf-viewer2.js
@@ -1,5 +1,5 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
-import 'https://unpkg.com/tonysoft@1.57.10/pdfMozilla/pdfMozilla.js';
+import 'https://unpkg.com/tonysoft@1.57.12/pdfMozilla/pdf.js';
 
 
 /**
